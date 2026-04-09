f5447e6aaaf01f64b59c1b0785c94aff23057e61
ponomarev.dv <graiwers@gmail.com>
2026-04-09 16:38:50 +0400

    tests authorization

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,21 +1,30 @@
-from os import name
-
 from playwright.sync_api import Page, expect
+from faker import Faker
 
-email = 'Bob2@mail.ru'
+shop = "https://demowebshop.tricentis.com/"
+email = 'Bob@mail.ru'
 password = 'some_pass'
 
+fake = Faker()
+
+def log_in(page: Page, email: email, password: password) -> None:
+    page.locator(".email").click()
+    page.locator(".email").fill(email)
+    page.locator(".password").click()
+    page.locator(".password").fill(password)
+    page.locator(".login-button").click()
+
 def test_register(page: Page) -> None:
-    page.goto("https://demowebshop.tricentis.com/")
+    page.goto(shop)
     page.locator(".ico-register").click()
 
     page.locator('#gender-male').click()
     page.locator('#FirstName').click()
-    page.locator('#FirstName').fill('Bob')
+    page.locator('#FirstName').fill(fake.name())
     page.locator('#LastName').click()
-    page.locator('#LastName').fill('Stivenson')
+    page.locator('#LastName').fill(fake.last_name())
     page.locator('#Email').click()
-    page.locator('#Email').fill(email)
+    page.locator('#Email').fill(fake.email())
     page.locator('#Password').click()
     page.locator('#Password').fill(password)
     page.locator('#ConfirmPassword').click()
@@ -23,38 +32,41 @@ def test_register(page: Page) -> None:
 
     page.locator('#register-button').click()
 
-
 def test_login(page: Page) -> None:
-    page.goto("https://demowebshop.tricentis.com/")
+    page.goto(shop)
     page.locator(".ico-login").click()
 
-    page.locator(".email").click()
-    page.locator(".email").fill(email)
-    page.locator(".password").click()
-    page.locator(".password").fill(password)
-
-    page.locator(".login-button").click()
+    log_in(page,email,password)
 
     expect(page.locator("body")).to_contain_text(email)
 
 def test_login_fail(page: Page) -> None:
-    page.goto("https://demowebshop.tricentis.com/")
+    page.goto(shop)
     page.locator(".ico-login").click()
 
-    page.locator(".email").click()
-    page.locator(".email").fill("Some@mail.ru")
-    page.locator(".password").click()
-    page.locator(".password").fill("1111")
-
-    page.locator(".login-button").click()
+    log_in(page,email,'111')
 
     expect(page.locator("body")).to_contain_text("Login was unsuccessful.")
 
 def test_forgot_password(page: Page) -> None:
-    pass
+    page.goto(shop + 'login')
+    page.locator('.forgot-password').click()
+    page.get_by_role("link", name="Forgot password?").click()
+    expect(page).to_have_url(shop + 'passwordrecovery')
 
-def test_remember_login(page: Page) -> None:
-    pass
+    page.locator(".email").click()
+    page.locator(".email").fill(email)
+    page.get_by_role("button", name="Recover").click()
+
+    expect(page.locator("body")).to_contain_text("Email with instructions has been sent to you.")
 
 def test_logout(page: Page) -> None:
-    pass
+    page.goto(shop)
+    page.locator(".ico-login").click()
+
+    log_in(page, email, password)
+
+    expect(page.locator("body")).to_contain_text(email)
+
+    page.locator('.ico-logout').click()
+    expect(page.get_by_role("link", name="Log in")).to_be_visible()
---
+++ tests/test_catalog.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page, expect
+
+def test_cards(page: Page) -> None:
+    pass
+
+def test_pages_catalog(page: Page) -> None:
+    pass
+
+def test_sort(page: Page) -> None:
+    pass
+
+def test_filters(page: Page) -> None:
+    pass
+
+def test_product_card(page: Page) -> None:
+    pass
