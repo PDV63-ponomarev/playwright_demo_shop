ab8cfb846ca1a7a2ebb20ae7d15c215b09fb19ed
Dima <graiwers@gmail.com>
2026-04-12 17:02:35 +0400

    get test search

---
+++ tests/test_search.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import Page, expect
+
+def test_search_for_word(page: Page) -> None:
+    pass
+
+def test_search_for_help_word(page: Page) -> None:
+    pass
+
+def test_search_for_empty(page: Page) -> None:
+    pass
+
+def test_clear_search(page: Page) -> None:
+    pass
