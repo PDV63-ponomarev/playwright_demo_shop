d0707f881cf422defba13ab7a27f0d606a9abeb8
Dima <graiwers@gmail.com>
2026-04-13 19:25:49 +0400

    test search

---
+++ tests/test_cart.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page, expect
+
+def test_give_item_in_cart(page: Page) -> None:
+    pass
+
+def test_change_item_in_cart(page: Page) -> None:
+    pass
+
+def test_delete_item_in_cart(page: Page) -> None:
+    pass
+
+def test_summ_price_in_cart(page: Page) -> None:
+    pass
+
+def test_making_order(page: Page) -> None:
+    pass
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,13 +1,40 @@
 from playwright.sync_api import Page, expect
 
+word = 'comput'
+fall_word = 'some'
+
 def test_search_for_word(page: Page) -> None:
-    pass
+    page.goto('https://demowebshop.tricentis.com')
+    page.locator('.search-box-text').fill(word)
+    page.get_by_role("button", name="Search").click()
+
+    expect(page.locator('.item-box').first).to_be_visible()
+
+    for item in page.locator('.item-box').all():
+        title_text = item.locator('.product-title').text_content()
+        assert word.lower() in title_text.lower()
+
+def test_search_for_fall_word(page: Page) -> None:
+    page.goto('https://demowebshop.tricentis.com')
+    page.locator('.search-box-text').fill(fall_word)
+    page.get_by_role("button", name="Search").click()
+
+    expect(page.locator('.result')).to_contain_text('No products were found that matched your criteria.')
 
 def test_search_for_help_word(page: Page) -> None:
-    pass
+    page.goto('https://demowebshop.tricentis.com')
+    page.locator('.search-box-text').fill(word)
+    page.locator("#ui-id-2").click()
+
+    title_text = page.locator('#product-details-form').text_content()
+    assert word.lower() in title_text.lower()
 
 def test_search_for_empty(page: Page) -> None:
-    pass
+    page.goto('https://demowebshop.tricentis.com')
 
-def test_clear_search(page: Page) -> None:
-    pass
+    def handle_dialog(dialog):
+        assert 'Please enter some search keyword' in dialog.message  # Сохраняем сообщение
+        dialog.accept()
+    page.on("dialog", handle_dialog)
+    page.get_by_role("button", name="Search").click()
+
